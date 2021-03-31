8fa4dde6ebd5dd8974084b7d12561cda714c13c6
joaopcbertoldo <24547377+joaopcbertoldo@users.noreply.github.com>
2021-03-31 22:14:42 +0200

    update update script

--- scripts/update.py
+++ scripts/update.py
@@ -6,9 +6,9 @@ from dataclasses import dataclass, field
 from logging import getLogger
 from pathlib import Path
 from shutil import copyfile
-from typing import Optional
+from typing import Optional, Dict
 
-DEFAULT_WORKDIR = Path(__file__).parent.parent
+DEFAULT_WORKDIR = Path(__file__).parent.parent.absolute()
 
 logging.basicConfig(level=logging.DEBUG)
 logger = getLogger(__file__)
@@ -42,7 +42,7 @@ class File:
 
     def update(self):
 
-        logger.debug(f"updating `{self.destination.name}`")
+        logger.debug(f"copying `{self.origin.name}` --> `{self.destination.name}`")
 
         if self.destination.exists():
             logger.debug(f"holding the existing file")
@@ -54,7 +54,7 @@ class File:
 
     def delete_destination_hold(self):
 
-        logger.debug(f"deleting hold file of `{self.destination}`")
+        logger.debug(f"deleting the file `{self.destination_hold}`")
 
         if not self.destination_hold.exists():
             logger.debug(f"it doesn't exist, skipping...")
@@ -88,24 +88,30 @@ def goto_workdir(workdir: Optional[str]):
     os.chdir(workdir)
 
 
-def main_files(workdir: Optional[str], mapping: Optional[str]):
-
-    goto_workdir(workdir)
-
-    if mapping is not None:
-        raise NotImplementedError(f"i haven't implemented mapping != None yet (:")
-
-    if mapping is None:
-        logger.info("using default files mapping")
-        mapping = get_files_default_mapping()
-
+def _files_update(mapping: Dict[str, File]) -> None:
     for key, file in mapping.items():
-
         logger.info(f"updating `{key}`")
         file.update()
 
 
-def main_purge_holds(workdir: Optional[str], mapping: Optional[str]):
+def _purge_holds(mapping: Dict[str, File]) -> None:
+    for key, file in mapping.items():
+        logger.info(f"purge the hold of `{key}`")
+        file.delete_destination_hold()
+
+
+files_command = "files"
+purge_holds_command = "purge-holds"
+
+
+def main_files(files_command: str, workdir: Optional[str], mapping: Optional[str]):
+    """
+
+    :param files_command:
+    :param workdir:
+    :param mapping:
+    :return:
+    """
 
     goto_workdir(workdir)
 
@@ -116,9 +122,14 @@ def main_purge_holds(workdir: Optional[str], mapping: Optional[str]):
         logger.info("using default files mapping")
         mapping = get_files_default_mapping()
 
-    for key, file in mapping.items():
-        logger.info(f"purge the hold of `{key}`")
-        file.delete_destination_hold()
+    if files_command is None:
+        _files_update(mapping)
+
+    elif files_command == purge_holds_command:
+        _purge_holds(mapping)
+
+    else:
+        NotImplementedError(f"{files_command=}")
 
 
 if __name__ == "__main__":
@@ -130,39 +141,46 @@ if __name__ == "__main__":
 
     parser.add_argument("--workdir", type=str, default=None, help="An absolute path in the file system. The script changes to this directory and *relative paths* are taken from there. Default: root of the repository.")
 
-    commands = parser.add_subparsers(dest="command", help="sub-command help")
+    commands = parser.add_subparsers(dest="command")
 
     # ============================================= files ============================================
-    files_command = "files"
-    files_parser = commands.add_parser(files_command, description="update files in the /files directory such as my cv", help="files help")
+    files_parser = commands.add_parser(files_command, description="update files in the /files directory such as my cv", help="stuff in the directory /files")
 
     files_parser.add_argument("--mapping", type=str, default=None, help=f"An absolute path in the file system. An .ini-like file mapping the origin-> destination of the files to be updated. Defaul: defined in `{__file__}`.")
 
+    files_commands = files_parser.add_subparsers(dest="files_command")
+
     # ============================================= purge-holds ======================================
 
-    purge_holds_command = "purge-holds"
-    purge_holds_parser = commands.add_parser(purge_holds_command, description="get rid of the hold_ files in the /files directory")
-
-    purge_holds_parser.add_argument("--mapping", type=str, default=None, help=f"An absolute path in the file system. An .ini-like file mapping the origin-> destination of the files to be updated. Defaul: defined in `{__file__}`.")
+    purge_holds_parser = files_commands.add_parser(
+        purge_holds_command,
+        description="get rid of the hold_ files in the /files directory",
+    )
 
     # ============================================= exec ======================================
 
     args = parser.parse_args()
+    logger.debug(f"{args=}")
 
     command2main_mapping = {
         files_command: main_files,
-        purge_holds_command: main_purge_holds,
     }
 
-    logger.debug(f"{args.command=}")
-    main = command2main_mapping[args.command]
-    logger.debug(f"{main.__name__=}")
+    command = args.command
 
+    if command is None:
+        logger.warning("No command given. Call option -h for help.")
+        exit(0)
+
+    logger.debug(f"{command=}")
+    main = command2main_mapping[command]
+    logger.debug(f"{main.__name__=}")
     del args.command
+
     args_dict = vars(args)
+    logger.debug(f"{args=}")
 
     try:
-        logger.debug(f"{args_dict=}")
         main(**args_dict)
 
     except Exception as ex:
